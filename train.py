3a117e47bcad8113d04869979fbcfe262dc51495
oykusahin <40883071+oykusahin@users.noreply.github.com>
2020-04-02 04:04:00 +0300

    bugs fixed until #Bookmark

--- train.py
+++ train.py
@@ -1,21 +1,21 @@
 import tensorflow as tf
 from data_loader import data_loader
 
+print(tf.__version__)
+tf.disable_v2_behavior()
 
 images, labels = data_loader()
+images= tf.convert_to_tensor(images, dtype='float32', name='X_train')
+labels= tf.convert_to_tensor(labels, dtype='float32', name='Y_train')
 
 training_iters = 10 
 learning_rate = 0.001 
 batch_size = 128
-
 n_input = 80
 n_classes = 8
 
-
-
-x = tf.placeholder("float", [None, 80,80,1])
+x = tf.placeholder("float", [None, 80,80,3])
 y = tf.placeholder("float", [None, n_classes])
-
 
 
 def conv2d(x, W, b, strides=1):
@@ -25,83 +25,86 @@ def conv2d(x, W, b, strides=1):
     return tf.nn.relu(x) 
 
 def maxpool2d(x, k=2):
-    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],padding='SAME')
+    return tf.nn.max_pool2d(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],padding='SAME')
 
+#to later pass your training data in when you run your session.
+def create_placeholders(n_s, n_xc, n_y):
+    X = tf.placeholder(tf.float32, [None, n_s, n_s, n_xc], name="X")
+    Y = tf.placeholder(tf.float32, [None, n_y], name="Y")    
+    return X, Y
 
-def conv_net(x, weights, biases):  
-    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+def initialize_parameters():
+    print("initialize_parameters")
+    with tf.compat.v1.variable_scope("init",reuse=tf.compat.v1.AUTO_REUSE):
+        W0  = tf.get_variable('wc0' , shape = [3, 3, 3, 32])
+        W1  = tf.get_variable('wc1' , shape = [3, 3, 32, 64])
+        W2  = tf.get_variable('wc2' , shape = [3, 3, 64, 128])
+        WD1 = tf.get_variable('wd1' , shape = [10*10*128, 128])
+        out = tf.get_variable('out', shape=[128, 1])
+        
+        b0  = tf.get_variable('b0' , shape = (32) )
+        b1  = tf.get_variable('b1' , shape = (64) )
+        b2  = tf.get_variable('b2' , shape = (128))
+        bd1 = tf.get_variable('bd1', shape = (128))
+        b_o = tf.get_variable('bo' , shape = (1))
+    
+
+        parameters = {"W0" : W0, "b0" : b0,
+                      "W1" : W1, "b1" : b1,
+                      "W2" : W2, "b2" : b2,
+                      "WD1": WD1,"bd1": bd1,
+                      "out": out,"b_o": b_o
+                      }
+    return parameters 
+
+def forward_propagation(X, parameters):
+    print("forward_prop")
+    W0  = parameters['W0']
+    b0  = parameters['b0']
+    W1  = parameters['W1']
+    b1  = parameters['b1']
+    W2  = parameters['W2']
+    b2  = parameters['b2']
+    WD1 = parameters['WD1']
+    bd1 = parameters['bd1']
+    o = parameters['out']
+    b_o = parameters['b_o']
+    
+    conv1 = conv2d(X, W0, b0)
     conv1 = maxpool2d(conv1, k=2)
 
-    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    conv2 = conv2d(conv1, W1, b1)
     conv2 = maxpool2d(conv2, k=2)
 
-    conv3 = conv2d(conv2, weights['wc3'], biases['bc3'])
+    conv3 = conv2d(conv2, W2, b2)
     conv3 = maxpool2d(conv3, k=2)
-
-    fc1 = tf.reshape(conv3, [-1, weights['wd1'].get_shape().as_list()[0]])
-    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    
+    fc1 = tf.reshape(conv3, [-1, WD1.get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, WD1), bd1)
     fc1 = tf.nn.relu(fc1)
-    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
-    return out
+    print(fc1.shape)
+    
+    outL = tf.add(tf.matmul(fc1, o), b_o)
+    print(outL.shape)
+    
+    return outL
 
+parameters = initialize_parameters()
+deneme = forward_propagation(images, parameters)
 
-weights = {
-    'wc1': tf.get_variable('W0', shape=(3,3,1,32), initializer=tf.contrib.layers.xavier_initializer()), 
-    'wc2': tf.get_variable('W1', shape=(3,3,32,64), initializer=tf.contrib.layers.xavier_initializer()), 
-    'wc3': tf.get_variable('W2', shape=(3,3,64,128), initializer=tf.contrib.layers.xavier_initializer()), 
-    'wd1': tf.get_variable('W3', shape=(4*4*128,128), initializer=tf.contrib.layers.xavier_initializer()), 
-    'out': tf.get_variable('W6', shape=(128,n_classes), initializer=tf.contrib.layers.xavier_initializer()), 
-}
-biases = {
-    'bc1': tf.get_variable('B0', shape=(32), initializer=tf.contrib.layers.xavier_initializer()),
-    'bc2': tf.get_variable('B1', shape=(64), initializer=tf.contrib.layers.xavier_initializer()),
-    'bc3': tf.get_variable('B2', shape=(128), initializer=tf.contrib.layers.xavier_initializer()),
-    'bd1': tf.get_variable('B3', shape=(128), initializer=tf.contrib.layers.xavier_initializer()),
-    'out': tf.get_variable('B4', shape=(10), initializer=tf.contrib.layers.xavier_initializer()),
-}
+def compute_cost(Z3, Y):    
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
+    
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+    return cost
 
-pred = conv_net(x, weights, biases)
+tf.reset_default_graph()
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-
-#Here you check whether the index of the maximum value of the predicted image is equal to the actual labelled image. and both will be a column vector.
-correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-
-#calculate accuracy across all the given images and average them out. 
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-# Initializing the variables
-init = tf.global_variables_initializer()
-
+###Bookmark
 with tf.Session() as sess:
-    sess.run(init) 
-    train_loss = []
-    test_loss = []
-    train_accuracy = []
-    test_accuracy = []
-    summary_writer = tf.summary.FileWriter('./Output', sess.graph)
-    for i in range(training_iters):
-        for batch in range(len(train_X)//batch_size):
-            batch_x = train_X[batch*batch_size:min((batch+1)*batch_size,len(train_X))]
-            batch_y = train_y[batch*batch_size:min((batch+1)*batch_size,len(train_y))]    
-            # Run optimization op (backprop).
-                # Calculate batch loss and accuracy
-            opt = sess.run(optimizer, feed_dict={x: batch_x,
-                                                              y: batch_y})
-            loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x,
-                                                              y: batch_y})
-        print("Iter " + str(i) + ", Loss= " + \
-                      "{:.6f}".format(loss) + ", Training Accuracy= " + \
-                      "{:.5f}".format(acc))
-        print("Optimization Finished!")
-
-        # Calculate accuracy for all 10000 mnist test images
-        test_acc,valid_loss = sess.run([accuracy,cost], feed_dict={x: test_X,y : test_y})
-        train_loss.append(loss)
-        test_loss.append(valid_loss)
-        train_accuracy.append(acc)
-        test_accuracy.append(test_acc)
-        print("Testing Accuracy:","{:.5f}".format(test_acc))
-    summary_writer.close()
+    X, Y = create_placeholders(12288, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(images, parameters)
+    cost = compute_cost(Z3, labels)
+    print("cost = " + str(cost))
